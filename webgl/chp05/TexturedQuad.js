9763a906bce91b51efc61c4f199dfa69ae23c1c1
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2023-05-08 13:42:03 +0800

    wip(chp05): texture

---
+++ webgl/chp05/TexturedQuad.js
@@ -0,0 +1,112 @@
+let VSHADER_SOURCE = /* glsl */ `
+  attribute vec4 a_Position;
+  attribute vec2 a_TexCoord;
+  varying vec2 v_TexCoord;
+  void main() {
+    gl_Position = a_Position;
+    v_TexCoord = a_TexCoord;
+  }
+`
+
+let FSHADER_SOURCE = /* glsl */`
+  #ifdef GL_ES
+  precision mediump float; // 精度限制
+  #endif
+  uniform sampler2D u_Sampler;
+  varying vec2 v_TexCoord;
+  void main() {
+    gl_FragColor = texture2D(u_Sampler, v_TexCoord);
+  }
+`
+
+function main() {
+  const canvas = document.getElementById('webgl');
+  const gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to initialize shaders.');
+    return;
+  }
+  
+  let n = initVertexBuffers(gl)
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  // gl.clear(gl.COLOR_BUFFER_BIT);
+  // gl.drawArrays(gl.POINTS, 0, n);
+  if (!initTextures(gl, n)) {
+    console.log('Failed to initialize texture.');
+  }
+}
+
+function initVertexBuffers(gl) {
+  const verticesTexCoords = new Float32Array([
+    -0.5, 0.5, 0, 1.0,
+    -0.5, -0.5, 0, 0,
+    0.5, 0.5, 1.0, 1.0,
+    0.5, -0.5, 1.0, 0,
+  ]);
+  let n = 4;
+  let vertexTexCoordBuffer = gl.createBuffer();
+  if (!vertexTexCoordBuffer) {
+    console.log('Failed to create the buffer object');
+    return -1;
+  }
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexTexCoordBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, verticesTexCoords, gl.STATIC_DRAW);
+
+  var FSIZE = verticesTexCoords.BYTES_PER_ELEMENT;
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return -1;
+  }
+
+  // Assign the buffer object to a_Position variable
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 4, 0);
+  // Enable the assignment to a_Position variable
+  gl.enableVertexAttribArray(a_Position);
+
+  var a_TexCoord = gl.getAttribLocation(gl.program, 'a_TexCoord');
+  gl.vertexAttribPointer(a_TexCoord, 2, gl.FLOAT, false, FSIZE * 4, FSIZE * 2);
+  gl.enableVertexAttribArray(a_TexCoord);
+  return n;
+}
+
+function initTextures(gl, n) {
+  // 创建纹理对象
+  let texture = gl.createTexture();
+  // 获取u_Sampler的存储位置
+  let u_Sampler = gl.getUniformLocation(gl.program, 'u_Sampler');
+
+  // 创建image对象
+  let image = new Image();
+  image.onload = function() {
+    loadTexture(gl, n, texture, u_Sampler, image);
+    console.log('image loaded');
+  }
+
+  image.src = '../../images/sky.jpg';
+  return true;
+}
+
+function loadTexture(gl, n, texture, u_Sampler, image) {
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // 对纹理图像进行Y轴反转
+  // 开启0号纹理单元
+  gl.activeTexture(gl.TEXTURE0);
+  // 向target绑定纹理对象
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // 配置纹理参数
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  // 配置纹理对象
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+
+  // 将0号纹理传递给着色器
+  gl.uniform1i(u_Sampler, 0);
+  gl.clear(gl.COLOR_BFFER_BIT);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, n); // 绘制矩形
+}
